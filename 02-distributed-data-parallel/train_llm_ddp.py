ffd6e2242d0a7e62a4a2c41435e0cd21285731a3
zazer0@iscsz <zazer.unsw@gmail.com>
2025-02-25 01:46:17 +0000

    chore(02): wandb name

--- 02-distributed-data-parallel/train_llm_ddp.py
+++ 02-distributed-data-parallel/train_llm_ddp.py
@@ -121,7 +121,7 @@ def main():
 
     if rank == 0:
         wandb.init(
-            project="distributed-training-guide",
+            project="distrib-training",
             dir=exp_dir,
             name=args.experiment_name,
             id=args.experiment_name,
